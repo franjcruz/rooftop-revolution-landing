3fcd0f735b9fa6e70552a6f9d9edc8b4c8098721
Fran Cruz <fran.cruz@es.abb.com>
2024-02-23 14:34:50 +0100

    Init project

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+
+// import Margarita from "@holaluz/margarita";
+import App from "./App.vue";
+
+const app = createApp(App);
+// app.use(Margarita);
+app.mount("#app");
